1d698f1f87fd5947f27fac9e96117b90e9f656ff
Chris Pirillo <pirillo.chris@gmail.com>
2014-11-05 11:58:26 -0500

    v.02
    Added color
    Screen size increased to 400x400
    Functional.

--- life.py
+++ life.py
@@ -11,30 +11,36 @@ def printAll():
         for x in range (0,100):
                 for y in range (0,100):
                         if normGrid[x][y] == 1:
-                            points.append(pygame.Rect(2*x,2*y,2*(x+1),2*(y+1)))
+                            points.append(pygame.Rect(4*x,4*y,4*2,4*2))
         surface.fill((0,0,0))
         for rec in points:
-            pygame.draw.rect(surface, (100,100,100), rec, 0)
+            pygame.draw.rect(surface, (255,0,0), rec, 0)
         pygame.display.update()
 
             
 #Takes in current neighbors and determines if it should live through this generation
 def determineLife(x,y):
         neighbors = checkNeighbors(x,y)
+        #if neighbors != 0 and neighbors != 1 and neighbors != 2 and neighbors != 3:
+            #print("LIVING: ", neighbors)
         if neighbors == 2 or neighbors == 3:
-                return True
-        return False
+                return 1
+        else:
+            return 0
 
 def determineDLife(x,y):
         neighbors = checkNeighbors(x,y)
+        #if neighbors != 0 and neighbors != 1 and neighbors != 2 and neighbors != 3:
+            #print("DEAD: ", neighbors)
         if neighbors == 3:
-                return True
-        return False    
+                return 1
+        else:
+            return 0    
         
 # Check neighbors and update temp grid
 def checkNeighbors(x,y):
         global normGrid
-        neighbors = 0
+        neighbors = int(0)
         # print("x: %d  y: %d", x, y)
         if x != 0 and y != 0 and x != 99 and y != 99:
             if normGrid[x-1][y] == 1:
@@ -43,7 +49,7 @@ def checkNeighbors(x,y):
                     neighbors += 1
             if normGrid[x+1][y] == 1:
                     neighbors += 1
-            if normGrid[x][y+1] == 1:
+            if normGrid[x][y-1] == 1:
                     neighbors += 1
             if normGrid[x-1][y-1] == 1:
                     neighbors += 1
@@ -54,6 +60,7 @@ def checkNeighbors(x,y):
             if normGrid[x+1][y-1] == 1:
                     neighbors += 1
             return neighbors
+        
                     
         if x == 0:
             if y == 0:
@@ -183,40 +190,41 @@ def checkNeighbors(x,y):
                     neighbors += 1
             return neighbors
                     
-            if y == 99:
-                if normGrid[x-1][99] == 1:
-                    neighbors += 1
-                if normGrid[x][98] == 1:
-                    neighbors += 1
-                if normGrid[x][0] == 1:
-                    neighbors += 1
-                if normGrid[x+1][99] == 1:
-                    neighbors += 1
-                if normGrid[x-1][99] == 1:
-                    neighbors += 1
-                if normGrid[x][99] == 1:
-                    neighbors += 1
-                if normGrid[x][1] == 1:
-                    neighbors += 1
-                if normGrid[x+1][1] == 1:
-                    neighbors += 1
-                return neighbors
+        if y == 99:
+            if normGrid[x-1][99] == 1:
+                neighbors += 1
+            if normGrid[x][98] == 1:
+                neighbors += 1
+            if normGrid[x][0] == 1:
+                neighbors += 1
+            if normGrid[x+1][99] == 1:
+                neighbors += 1
+            if normGrid[x-1][99] == 1:
+                neighbors += 1
+            if normGrid[x][99] == 1:
+                neighbors += 1
+            if normGrid[x][1] == 1:
+                neighbors += 1
+            if normGrid[x+1][1] == 1:
+                neighbors += 1
+            return neighbors
+            
+        return neighbors
                     
 
 # One tick of the game, call to start game
 def tick():
-        tempLiving = np.zeros((100,100))
+        tempLiving = [[0 for i in range(100)] for i in range(100)]
         global normGrid
         for x in range (0,100):
                 for y in range (0,100):
                         if normGrid[x][y] != 1:
-                                if determineDLife(x,y):
-                                        tempLiving[x][y] = 1
+                                tempLiving[x][y] = determineDLife(x,y)
                         if normGrid[x][y] == 1:
-                                if determineLife(x,y):
-                                        tempLiving[x][y] = 1
+                                tempLiving[x][y] = determineLife(x,y)
         normGrid = tempLiving
         printAll()
+        time.sleep(.05)
         tick()
 
 def main():
@@ -230,10 +238,12 @@ def main():
     for x in range (0,100):
             for y in range (0,100):
                     if normGrid[x][y] == 1:
-                            points.append(pygame.Rect(2*x,2*y,2*(x+1),2*(y+1)))
+                            points.append(pygame.Rect(4*x,4*y,4*2,4*2))
+                    else:
+                        normGrid[x][y] == 0
     
     tick()
  
 surface = pygame.display.set_mode((400, 400), 0, 0)
-normGrid = np.random.randint(0,10, size=(100,100))
+normGrid = [[np.random.randint(10) for i in range(100)] for i in range(100)]
 main()
